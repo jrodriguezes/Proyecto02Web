5a6578f7b4a82a4ac247540f541c9aff17d13081
jeremy rodriguez <jeremy12troll@gmail.com>
2025-08-05 22:49:56 -0600

    Se agrego un range de boostrap con lo cual se exigia .js. Aca es donde se usa

---
+++ js/rangeBoostrap.js
@@ -0,0 +1,9 @@
+const rangeInput = document.getElementById("customRange4");
+const rangeOutput = document.getElementById("rangeValue");
+
+// Set initial value
+rangeOutput.textContent = rangeInput.value;
+
+rangeInput.addEventListener("input", function () {
+  rangeOutput.textContent = this.value;
+});
