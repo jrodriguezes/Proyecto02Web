ebd6a0e3b8053078e67b7fd47c110a13a9d93541
jeremy rodriguez <jeremy12troll@gmail.com>
2025-08-05 22:43:50 -0600

    Se modifico y agrego codigo para hacer la clase funcione para ambos html (userRegister y driverRegister).

--- js/register.js
+++ js/register.js
@@ -8,8 +8,10 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function storeUsers() {
+  const userId = document.getElementById("user-id").value;
   const userName = document.getElementById("first-name").value;
   const lastName = document.getElementById("last-name").value;
+  const birthdate = document.getElementById("birthdate").value;
   const email = document.getElementById("email").value;
   const registerPassword = document.getElementById("register-password").value;
   const repeatPassword = document.getElementById("repeat-password").value;
@@ -18,30 +20,77 @@ function storeUsers() {
   const state = document.getElementById("state").value;
   const city = document.getElementById("city").value;
   const phoneNumber = document.getElementById("phone-number").value;
+  const vehicleBrandInput = document.getElementById("vehicle-brand");
+  const vehicleBrand = vehicleBrandInput ? vehicleBrandInput.value : null;
+
+  const vehicleModelInput = document.getElementById("vehicle-model");
+  const vehicleModel = vehicleModelInput ? vehicleModelInput.value : null;
+
+  const vehicleYearInput = document.getElementById("rangeValue");
+  const vehicleYear = vehicleYearInput ? vehicleYearInput.value : null;
+
+  const plateNumberInput = document.getElementById("plate-number");
+  const plateNumber = plateNumberInput ? plateNumberInput.value : null;
 
   if (registerPassword === repeatPassword) {
-    const userData = {
-      userName: userName,
-      lastName: lastName,
-      email: email,
-      registerPassword: registerPassword,
-      address: address,
-      country: country,
-      state: state,
-      city: city,
-      phoneNumber: phoneNumber,
-    };
+    if (vehicleBrand == null) {
+      const userData = {
+        userId: userId,
+        userName: userName,
+        lastName: lastName,
+        email: email,
+        registerPassword: registerPassword,
+        address: address,
+        country: country,
+        state: state,
+        birthdate: birthdate,
+        city: city,
+        phoneNumber: phoneNumber,
+        role: "user",
+      };
 
-    let users = JSON.parse(localStorage.getItem("users"));
-    if (users) {
-      users.push(userData);
+      let users = JSON.parse(localStorage.getItem("users"));
+      if (users) {
+        users.push(userData);
+      } else {
+        users = [userData];
+      }
+
+      localStorage.setItem("users", JSON.stringify(users));
+      alert("User registration successful!");
+      window.location.href = "login.html";
+      return true;
     } else {
-      users = [userData];
-    }
+      const userData = {
+        userId: userId,
+        userName: userName,
+        lastName: lastName,
+        email: email,
+        registerPassword: registerPassword,
+        address: address,
+        country: country,
+        state: state,
+        birthdate: birthdate,
+        city: city,
+        phoneNumber: phoneNumber,
+        role: "driver",
+        vehicleBrand: vehicleBrand,
+        vehicleModel: vehicleModel,
+        vehicleYear: vehicleYear,
+        plateNumber: plateNumber,
+      };
+      let users = JSON.parse(localStorage.getItem("users"));
+      if (users) {
+        users.push(userData);
+      } else {
+        users = [userData];
+      }
 
-    localStorage.setItem("users", JSON.stringify(users));
-    alert("Registration successful!");
-    return true;
+      localStorage.setItem("users", JSON.stringify(users));
+      alert("Driver registration successful!");
+      window.location.href = "login.html";
+      return true;
+    }
   } else {
     alert("Passwords do not match. Please try again.");
     return false;
