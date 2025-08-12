6cfc485c654249ad2ed25e2121e5fc968f453614
jeremy rodriguez <jeremy12troll@gmail.com>
2025-08-11 21:25:22 -0600

    Se agrego una validacion, la cual no permite al usuario hacer login sin colocar una contrase;a

--- js/login.js
+++ js/login.js
@@ -10,12 +10,15 @@ document.addEventListener("DOMContentLoaded", function () {
 function verifyUser() {
   const userName = document.getElementById("username").value;
   const password = document.getElementById("login-password").value;
+  if (password === "") {
+    alert("Enter a password");
+    return false;
+  }
 
   const usersList = JSON.parse(localStorage.getItem("users")) || [];
 
-  for (const user of usersList) {
+  for (let user of usersList) {
     if (userName === user.userName && password === user.registerPassword) {
-      
       sessionStorage.setItem("activeUser", JSON.stringify(user));
       alert("Succesfully logged in");
       window.location.href = "myRides.html";
